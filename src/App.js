941d35854531e82b4ac555f4c3dec730cfdddefe
Filip Siderov <filip.siderov@sap.com>
2019-04-03 13:36:31 +0300

    Add shellbar to the application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import "@ui5/webcomponents/dist/ShellBar";
+import "@ui5/webcomponents/dist/ShellBarItem";
+import "@ui5/webcomponents/dist/Popover";
+import "@ui5/webcomponents/dist/Icon";
+import "@ui5/webcomponents/dist/Input";
 
+import logo from './assets/images/CompanyLogo.png';
+import profileImage from './assets/images/Avatar_4.png';
 class App extends Component {
+  constructor() {
+    super();
+
+    const today = new Date();
+    var displayDate = `${new Intl.DateTimeFormat('en-US', { weekday: 'long'}).format(today)}, ${new Intl.DateTimeFormat('en-US', { month: 'long'}).format(today)} ${today.getDate()}, ${today.getFullYear()}`
+
+    this.state = {
+      displayDate
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <ui5-shellbar
+        primary-title='Drone Hive'
+        secondary-title={this.state.displayDate}
+        logo={logo}
+        profile={profileImage}
+        >
+          <ui5-input data-ui5-slot="searchField"></ui5-input>
+          
+          <ui5-shellbar-item data-ui5-slot="items" id="connect" src="sap-icon://paper-plane" text="Connect"></ui5-shellbar-item>
+	        <ui5-shellbar-item data-ui5-slot="items" id="notifications" src="sap-icon://bell" text="Notifications"></ui5-shellbar-item>
+        </ui5-shellbar>
       </div>
     );
   }
