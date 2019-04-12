3c27cf25b1d43fd5f54c3f4e829d7d3d38059a28
Filip Siderov <filip.siderov@sap.com>
2019-04-12 10:27:03 +0300

    Fix build paths

---
+++ build/precache-manifest.167de433e7ca43b7075f6d3a85de2e48.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ffae6750d3b243e3f4cabe207af44a3",
+    "url": "/pages/ui5-webcomponents/fiori3-sample-app/static/media/Avatar_7.5ffae675.png"
+  },
+  {
+    "revision": "42923ba721bd18d458e8",
+    "url": "/pages/ui5-webcomponents/fiori3-sample-app/static/css/main.468ed54e.chunk.css"
+  },
+  {
+    "revision": "24f6344efd0df7e63e0b",
+    "url": "/pages/ui5-webcomponents/fiori3-sample-app/static/js/runtime~main.492a850a.js"
+  },
+  {
+    "revision": "4c6ad398c0c396d9c5fe",
+    "url": "/pages/ui5-webcomponents/fiori3-sample-app/static/js/2.6765bc5d.chunk.js"
+  },
+  {
+    "revision": "06edf48fd56689204c01c290b035e53a",
+    "url": "/pages/ui5-webcomponents/fiori3-sample-app/static/media/Avatar_3.06edf48f.png"
+  },
+  {
+    "revision": "65b4ac0ecc237414fb5b45ae720b0176",
+    "url": "/pages/ui5-webcomponents/fiori3-sample-app/static/media/Avatar_1.65b4ac0e.png"
+  },
+  {
+    "revision": "042274a57f504b221df74c3f0beed455",
+    "url": "/pages/ui5-webcomponents/fiori3-sample-app/static/media/Avatar_2.042274a5.png"
+  },
+  {
+    "revision": "aa194a45ccd67ccc324b1c889693612c",
+    "url": "/pages/ui5-webcomponents/fiori3-sample-app/static/media/Avatar_5.aa194a45.png"
+  },
+  {
+    "revision": "c69bceae1b63f90b95ca35338a8d97d4",
+    "url": "/pages/ui5-webcomponents/fiori3-sample-app/static/media/Avatar_6.c69bceae.png"
+  },
+  {
+    "revision": "42923ba721bd18d458e8",
+    "url": "/pages/ui5-webcomponents/fiori3-sample-app/static/js/main.d929f36f.chunk.js"
+  },
+  {
+    "revision": "ecb382add335848505bd2a213e34d5aa",
+    "url": "/pages/ui5-webcomponents/fiori3-sample-app/static/media/Avatar_8.ecb382ad.png"
+  },
+  {
+    "revision": "a8219243082420b3f09bf44be938dc7c",
+    "url": "/pages/ui5-webcomponents/fiori3-sample-app/static/media/Avatar_9.a8219243.png"
+  },
+  {
+    "revision": "55f6316441ec9f8d1ac5ced5a5ec35fa",
+    "url": "/pages/ui5-webcomponents/fiori3-sample-app/static/media/Avatar_10.55f63164.png"
+  },
+  {
+    "revision": "9c65c14bb69e562130d1482756512b61",
+    "url": "/pages/ui5-webcomponents/fiori3-sample-app/static/media/chart1.9c65c14b.png"
+  },
+  {
+    "revision": "ef63ac8b60fe5b5b01285432386ae909",
+    "url": "/pages/ui5-webcomponents/fiori3-sample-app/static/media/chart2.ef63ac8b.png"
+  },
+  {
+    "revision": "2920aa9a1b581017197044d760d43194",
+    "url": "/pages/ui5-webcomponents/fiori3-sample-app/static/media/Carousel1.2920aa9a.png"
+  },
+  {
+    "revision": "c415249b8c8a2bef4377c17a4c175080",
+    "url": "/pages/ui5-webcomponents/fiori3-sample-app/static/media/Avatar_4.c415249b.png"
+  },
+  {
+    "revision": "6a65ec605c763db277c7aa4e3f2bd515",
+    "url": "/pages/ui5-webcomponents/fiori3-sample-app/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.42ce0aed032b7e2e527893c069c80912.js"
+  "/pages/ui5-webcomponents/fiori3-sample-app/precache-manifest.167de433e7ca43b7075f6d3a85de2e48.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/pages/ui5-webcomponents/fiori3-sample-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
